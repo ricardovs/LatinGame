aa3757274cc67db03498beca3682d6b4650284e4
Ricardo Vieira da Silva <ric1500ric@gmail.com>
2023-10-21 18:07:26 -0300

    question func

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from game import DeclensionEndingGame
 class App():
     def __init__(self):
         self.status = 'InitialPage'
+        self.game = None
         self.clear()
 
     def run(self):
@@ -17,25 +18,26 @@ class App():
     def InitialPage(self):
         self.clear()
         print("Welcome to the Latin conjugation game !!")
-        while True:
-            print("Guess declension ending.")
-            option = input("Press Enter to continue (or quit)...")
-            self.status = "DeclensionPage"
-            if option == 'quit':
-                return False
-            else:
-                return True
-
+        print("Guess declension ending.")
+        option = input("Press Enter to new game or quit ...\n")
+        if option == 'quit':
+            return False
+        self.status = "DeclensionPage"
+        self.game = DeclensionEndingGame()
+        self.clear()
+        return True
 
     def DeclensionPage(self):
-        self.clear()
-        print("This is the declesion page !!")
-        game = DeclensionEndingGame()
-        game.newQuestion()
-        while True:
-            print("Give the declension ending for this case (or quit)")
-            option = input("R: ")
-            if option == "quit":
-                self.status = 'InitialPage'
-                return True
-
+        if not self.game.isWaiting():
+            self.game.newQuestion()
+            self.clear()
+        print("This is the guessing page !!")
+        print("Give the declension ending for this case (or quit)")
+        print(self.game.getQuestion())
+        option = input("R: ")
+        if option == "quit":
+            self.status = 'InitialPage'
+            return True
+        self.game.addAnswers(option)
+        input("Press Enter to continue (or quit)...")
+        return True
--- game.py
+++ game.py
@@ -1,20 +1,40 @@
 import json
 
-class Game:
-    def __init__(self):
-        pass
-    def getQuestion(self):
-        return ""
-    def check(self, awnser):
-        pass
-    def newQuestion(self):
-        pass
-    def isQuestionCompleted(self):
-        pass
-
-class DeclensionEndingGame(Game):
+class DeclensionEndingGame:
     def __init__(self):
         with open('data.json') as file:
             self.data = json.load(file)['declesionEnddings']
-    def newQuestion(self):
+        self.questions = []
+        self.waiting_answers = False
+
+    def getQuestion(self):
+        if not self.waiting_answers:
+            return ""
+        return self.questions[-1]['question']
+
+    def check(self, answer):
         pass
+
+    def newQuestion(self):
+        question = {
+            'question':'1st Declension, Genitive, Singular, Feminine',
+            'answers' : []
+        }
+        self.questions.append(question)
+        self.waiting_answers = True
+
+    def isQuestionCompleted(self):
+        return not self.waiting_answers
+
+    def isWaiting(self):
+        return self.waiting_answers
+
+    def addAnswers(self, answer):
+        if not self.waiting_answers:
+            return False
+        if not answer or answer.isspace():
+            return False
+        question = self.questions[-1]
+        question['answers'].append(answer)
+        return True
+
